0c749aacdebace86252bde7f735bacb56548df14
araki <araki@kdl.co.jp>
2021-07-19 19:20:33 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+/* eslint-disable jsx-a11y/alt-text */
+import taiga_image from './image/taiga.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <h2>たいがくんのLINE QR</h2>
+      </div>
+      <div className="image">
+        <img src={taiga_image} width="300" height="300"/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
